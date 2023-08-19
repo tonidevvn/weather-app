eb7b321f97b24321d8930dc1b810be076df5fa40
tonidevvn <tonidevvn@gmail.com>
2023-08-20 06:34:48 +0700

    1st commit to initialize project

---
+++ src/views/App.js
@@ -0,0 +1,13 @@
+import Logo from "../components/Logo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
